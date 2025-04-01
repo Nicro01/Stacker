f40e1b7ac79336807c3f60ed9dd7933ec0cf7f20
Nicro01 <nicro@edu.unifil.br>
2025-03-31 23:03:48 -0300

    New -> Project Path Nullable

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('port')->default('2025');
-            $table->string('projects_path');
+            $table->string('projects_path')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
