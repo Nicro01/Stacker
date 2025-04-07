6599445ec6ba234d64f9af56cd2e6980363d60f6
Nicro01 <nicro@edu.unifil.br>
2025-04-07 00:36:07 -0300

    Updated -> Mobile Panel Navbar

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -5,32 +5,32 @@
         <div class="flex w-full gap-6">
 
             <div
-                class="rounded-box bg-base-200 shadow-base fixed left-6 top-6 z-50 flex h-[calc(100vh-3rem)] w-[5vw] flex-col justify-between px-4 py-4 shadow-lg max-sm:hidden">
-                <nav class="flex flex-col items-center space-x-0 space-y-2">
+                class="rounded-box bg-base-200 shadow-base fixed bottom-5 left-1/2 z-50 flex w-[5vw] justify-between px-4 py-4 shadow-lg max-sm:w-[90%] max-sm:-translate-x-1/2 sm:left-6 sm:top-6 sm:h-[calc(100vh-3rem)] sm:flex-col">
+                <nav class="flex items-center space-x-2 sm:flex-col sm:space-x-0 sm:space-y-2">
                     <a draggable="false" data-tip="Packages"
-                        class="btn tooltip tooltip-right @if (Route::currentRouteName() == 'panel.home') bg-base-content text-base-100 @endif rounded-box inline-flex size-12 justify-center border-none"
+                        class="btn tooltip sm:tooltip-right @if (Route::currentRouteName() == 'panel.home') bg-base-content text-base-100 @endif rounded-box inline-flex size-12 justify-center border-none"
                         href="{{ route('panel.home') }}">
 
                         <x-heroicon-c-cube class="h-6 min-w-8" />
                     </a>
 
                     <a draggable="false" data-tip="Project Configurations"
-                        class="btn tooltip tooltip-right @if (Route::currentRouteName() == 'panel.configs') bg-base-content text-base-100 @endif rounded-box inline-flex size-12 justify-center border-none"
+                        class="btn tooltip sm:tooltip-right @if (Route::currentRouteName() == 'panel.configs') bg-base-content text-base-100 @endif rounded-box inline-flex size-12 justify-center border-none"
                         href="{{ route('panel.configs') }}">
 
                         <x-heroicon-c-wrench class="h-6 min-w-8" />
 
                     </a>
                 </nav>
-                <div class="flex flex-col items-center space-x-0 space-y-2">
+                <div class="flex items-center sm:flex-col sm:space-x-0 sm:space-y-2">
                     <a draggable="false" data-tip="Your Profile"
-                        class="btn @if (Route::currentRouteName() == 'profile') bg-base-content text-base-100 @endif tooltip tooltip-right rounded-box inline-flex size-12 justify-center border-none"
+                        class="btn @if (Route::currentRouteName() == 'profile') bg-base-content text-base-100 @endif tooltip sm:tooltip-right rounded-box inline-flex size-12 justify-center border-none"
                         href="{{ route('profile') }}">
                         <x-heroicon-s-user class="h-6 min-w-8" />
 
                     </a>
                     <a draggable="false" data-tip="Logout"
-                        class="btn tooltip tooltip-right rounded-box inline-flex size-12 justify-center border-none"
+                        class="btn tooltip sm:tooltip-right rounded-box inline-flex size-12 justify-center border-none"
                         href="{{ route('logout') }}">
 
                         <x-heroicon-s-arrow-left-start-on-rectangle class="h-6 min-w-8" />
