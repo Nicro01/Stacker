027051722fdad2f66ce103dfb6b7bc4dbe722786
Nicro01 <nicro@edu.unifil.br>
2025-04-07 01:34:58 -0300

    Updated -> Tablet Panel Navbar

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -5,7 +5,7 @@
         <div class="flex w-full gap-6">
 
             <div
-                class="rounded-box bg-base-200 shadow-base fixed bottom-5 left-1/2 z-50 flex w-[5vw] justify-between px-4 py-4 shadow-lg max-sm:w-[90%] max-sm:-translate-x-1/2 sm:left-6 sm:top-6 sm:h-[calc(100vh-3rem)] sm:flex-col lg:w-[8vw]">
+                class="rounded-box bg-base-200 shadow-base fixed bottom-5 left-1/2 z-50 flex w-[5vw] justify-between px-4 py-4 shadow-lg max-sm:w-[90%] max-sm:-translate-x-1/2 sm:left-6 sm:top-6 sm:h-[calc(100vh-3rem)] sm:flex-col md:w-[8vw]">
                 <nav class="flex items-center space-x-2 sm:flex-col sm:space-x-0 sm:space-y-2">
                     <a draggable="false" data-tip="Packages"
                         class="btn tooltip sm:tooltip-right @if (Route::currentRouteName() == 'panel.home') bg-base-content text-base-100 @endif rounded-box inline-flex size-12 justify-center border-none"
@@ -38,7 +38,7 @@
                     </a>
                 </div>
             </div>
-            <div class="flex-1 px-0 pe-[1.5vw] lg:ms-[14vw] 2xl:ms-[8vw]">
+            <div class="flex-1 px-0 pe-[1.5vw] md:ms-[14vw] 2xl:ms-[8vw]">
                 @yield('content')
 
                 @isset($slot)
